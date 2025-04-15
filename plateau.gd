c5a5d30bc73d95a490b0b04800ff23e74f84c608
joelmortel <136242917+joelmortel@users.noreply.github.com>
2025-04-14 21:10:40 -0400

    encore
    niaisage

--- plateau.gd
+++ plateau.gd
@@ -29,7 +29,6 @@ extends Node2D
 @onready var banane_22: Marker2D = $banane/banane_22
 @onready var banane_23: Marker2D = $banane/banane_23
 @onready var banane_24: Marker2D = $banane/banane_24
-@onready var banane_25: Marker2D = $banane/banane_25
 
 
 ## oranges
@@ -59,7 +58,6 @@ extends Node2D
 @onready var orange_22: Marker2D = $orange/orange_22
 @onready var orange_23: Marker2D = $orange/orange_23
 @onready var orange_24: Marker2D = $orange/orange_24
-@onready var orange_25: Marker2D = $orange/orange_25
 
 
 var orange_array = []
@@ -91,10 +89,10 @@ func placer_bonhomme(couleur: String, fiche: Array):
 func create_arrays():
 	var temp_banana = [banane_00, banane_01, banane_02, banane_03, banane_04, banane_05, banane_06, banane_07,
 	banane_08, banane_09, banane_10, banane_11, banane_12, banane_13, banane_14, banane_15, banane_16, banane_17,
-	banane_18, banane_19, banane_20, banane_21, banane_22, banane_23, banane_24, banane_25]
+	banane_18, banane_19, banane_20, banane_21, banane_22, banane_23, banane_24]
 	banana_array.append_array(temp_banana)
 	
 	var temp_orange = [orange_00, orange_01, orange_02, orange_03, orange_04, orange_05, orange_06, orange_07,
 	orange_08, orange_09, orange_10, orange_11, orange_12, orange_13, orange_14, orange_15, orange_16, orange_17,
-	orange_18, orange_19, orange_20, orange_21, orange_22, orange_23, orange_24, orange_25]
+	orange_18, orange_19, orange_20, orange_21, orange_22, orange_23, orange_24]
 	orange_array.append_array(temp_orange)
